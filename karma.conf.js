c17496fc3b9d65682d9362833839bff8134a20db
m8ig <mike@sitio.ru>
2017-05-09 23:23:37 +0400

    Karma config file

---
+++ karma.conf.js
@@ -0,0 +1,31 @@
+/* eslint-disable */
+module.exports = (config) => {
+  config.set({
+
+    basePath: './app',
+
+    files: [
+      '../node_modules/angular/angular.js',
+      '../node_modules/angular-animate/angular-animate.js',
+      '../node_modules/angular-resource/angular-resource.js',
+      '../node_modules/angular-mocks/angular-mocks.js',
+      '../node_modules/angular-route/angular-route.js',
+      'assets/js/*!(.module|.spec|.component).js',
+      'assets/js/**/*.module.js',
+      'assets/js/**/*.component.js',
+      'assets/js/**/*.spec.js'
+    ],
+
+    autoWatch: true,
+
+    frameworks: ['jasmine'],
+
+    browsers: ['Chrome'],
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-jasmine'
+    ]
+
+  });
+};
