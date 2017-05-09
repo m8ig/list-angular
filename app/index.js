e435f0fc05d8c7810d724ddac656e881c925b02e
m8ig <mike@sitio.ru>
2017-05-09 23:26:04 +0400

    Index file

---
+++ app/index.js
@@ -0,0 +1,9 @@
+// import js
+import 'angular';
+import './assets/js/app.module';
+import './assets/js/list/list.module';
+import './assets/js/list/list.component';
+
+
+// import styles
+import './assets/sass/app.scss';
