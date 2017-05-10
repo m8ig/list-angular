60d3aaa9a0078ba857dbf5b4b010199d6b4cf0b9
m8ig <mike@sitio.ru>
2017-05-10 18:47:58 +0400

    Get data from API

--- app/assets/js/list/list.component.js
+++ app/assets/js/list/list.component.js
@@ -2,26 +2,33 @@
 angular
   .module('list')
   .component('list', {
-    controller: function ListController() {
-      this.orderProp = 'age';
+    controller: [
+      '$http',
+      function ListController($http) {
+        this.orderProp = 'age';
 
-      this.phones = [
-        {
-          name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.',
-          age: 1,
-        },
-        {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.',
-          age: 2,
-        },
-        {
-          name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.',
-          age: 3,
-        },
-      ];
-    },
+        const param = {
+          key: '70d1b60c16444f8e9c8112926171005',
+          q: '37.773972,-122.431297',
+          days: '10',
+        };
+
+        function handleTransform(obj) {
+          const array = Object.keys(obj).map((key) => {
+            return `${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`;
+          });
+          return array.join('&');
+        }
+
+        $http
+          .get(`https://api.apixu.com/v1/forecast.json?${handleTransform(param)}`)
+          .then((response) => {
+            this.forecasts = response.data.forecast.forecastday;
+          })
+          .catch((reason) => {
+            console.log(reason.statusText);
+          });
+      },
+    ],
     templateUrl: './list/list.template.html',
   });
--- app/assets/js/list/list.component.spec.js
+++ app/assets/js/list/list.component.spec.js
@@ -3,15 +3,46 @@ describe('list', () => {
 
   // Test the controller
   describe('ListController', () => {
-    beforeEach(inject(($componentController) => {
-      return ctrl = $componentController('list');
+    let $httpBackend;
+    let ctrl;
+
+    beforeEach(inject(($componentController, _$httpBackend_) => {
+      $httpBackend = _$httpBackend_;
+
+      $httpBackend
+        .expectGET('https://api.apixu.com/v1/forecast.json?key=70d1b60c16444f8e9c8112926171005&q=37.773972%2C-122.431297&days=10')
+        .respond({
+          forecast: {
+            forecastday: [
+              {
+                date: '2017-05-10',
+              },
+              {
+                date: '2017-05-10',
+              },
+            ],
+          },
+        });
+
+      ctrl = $componentController('list');
     }));
 
-    it('should create a `phones` model with 3 phones', () => {
-      expect(ctrl.phones.length).toBe(3);
+    it('should create a `phones` property with 2 phones fetched with `$http`', () => {
+      expect(ctrl.forecasts).toBeUndefined();
+
+      $httpBackend.flush();
+
+      expect(ctrl.forecasts).toEqual([
+        {
+          date: '2017-05-10',
+        },
+        {
+          date: '2017-05-10',
+        },
+      ]);
     });
 
-    it('should set a default value for the `orderProp` model', () => {
+    it('should set a default value for the `orderProp` property', () => {
       expect(ctrl.orderProp).toBe('age');
     });
   });
